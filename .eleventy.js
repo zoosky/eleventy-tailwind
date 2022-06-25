9161610c84bb792563da9869b9151f53c712c060
Zoo Sky <wortblau@gmail.com>
2022-06-25 08:33:21 +0200

    add watch on css too

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Return your Object options:
-  eleventyConfig.addPassthroughCopy("./src/assets/**/*");
+    eleventyConfig.addPassthroughCopy("./src/assets/**/*");
+    eleventyConfig.addWatchTarget("./src/assets/**/*");
     return {
         dir: {
             input: "src",
