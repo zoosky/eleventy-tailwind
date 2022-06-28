2dec0587014a2ba54b7437718fa21eec1a5fa3b0
Zoo Sky <wortblau@gmail.com>
2022-06-28 07:06:13 +0200

    add plugin, list style square

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
     content: ["./public/**/*.{html,njk}"],
   theme: {
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    },
     extend: {}, 
   },
-  plugins: [],
+  variants: {},
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp'),require('@tailwindcss/aspect-ratio'),require('@tailwindcss/typography'),],
 }
