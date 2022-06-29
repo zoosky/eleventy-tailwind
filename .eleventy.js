a5cf8fa66e22e0ea4faf7d4d9dc9ffd97679a579
Zoo Sky <wortblau@gmail.com>
2022-06-29 22:33:52 +0200

    extend and override, timestamped css, js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
-module.exports = function(eleventyConfig) {
+const now = String(Date.now())
+
+module.exports = function (eleventyConfig) {
   // Return your Object options:
     eleventyConfig.addPassthroughCopy("./src/assets/**/*");
     eleventyConfig.addWatchTarget("./src/assets/**/*");
+    eleventyConfig.addShortcode('version', function () {
+        return now
+    })
+    
     return {
         dir: {
             input: "src",
